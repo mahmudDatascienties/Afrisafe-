8ae402cf297237dea945e407fa5fe58771235de5
Mahmud Mukhtar Hassan <mahmudmukhtarhassan@gmail.com>
2026-07-21 22:27:10 +0100

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -6,3 +6,8 @@ Base.metadata.create_all(bind=engine)
 from app.routes.auth import router as auth_router
 
 app.include_router(auth_router)
+
+from app.database import Base
+from app.database import engine
+
+import app.models.user
