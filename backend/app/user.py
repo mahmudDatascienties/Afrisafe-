3cfa85ba94ffb59813157c10492fc258f904031c
Mahmud Mukhtar Hassan <mahmudmukhtarhassan@gmail.com>
2026-07-21 21:52:29 +0100

    Update user.py

--- backend/app/user.py
+++ backend/app/user.py
@@ -1,1 +1,24 @@
+from pydantic import BaseModel, EmailStr
 
+
+class UserRegister(BaseModel):
+
+    fullname: str
+
+    email: EmailStr
+
+    password: str
+
+
+class UserLogin(BaseModel):
+
+    email: EmailStr
+
+    password: str
+
+
+class Token(BaseModel):
+
+    access_token: str
+
+    token_type: str
